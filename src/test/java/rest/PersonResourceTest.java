584335494da870df3f33d9cef40641b99e0fe486
Thomas Overgaard <Thomasovergaard@hotmail.com>
2021-10-05 09:02:10 +0200

    dumme peter

--- src/test/java/rest/PersonResourceTest.java
+++ src/test/java/rest/PersonResourceTest.java
@@ -51,10 +51,12 @@ public class PersonResourceTest {
 
     @BeforeAll
     public static void setUpClass() {
+        //This method must be called before you request the EntityManagerFactory
         EMF_Creator.startREST_TestWithDB();
         emf = EMF_Creator.createEntityManagerFactoryForTest();
 
         httpServer = startServer();
+        //Setup RestAssured
         RestAssured.baseURI = SERVER_URL;
         RestAssured.port = SERVER_PORT;
         RestAssured.defaultParser = Parser.JSON;
@@ -62,19 +64,17 @@ public class PersonResourceTest {
 
     @AfterAll
     public static void closeTestServer() {
+        //System.in.read();
+
+        //Don't forget this, if you called its counterpart in @BeforeAll
         EMF_Creator.endREST_TestWithDB();
         httpServer.shutdownNow();
     }
 
     @BeforeEach
-<<<<<<< HEAD
-    void setUp() {
-        EntityManager em = emf.createEntityManager();
-=======
     public void setUp() {
         EntityManager em = emf.createEntityManager();
 
->>>>>>> c05468913c0cb9ea5599d5be78f2f2c1177b5dc0
         p1 = new Person("Mogens", "Glad", "m@g.dk");
         p2 = new Person("Peter", "Belly", "p@b.dk");
 
@@ -100,13 +100,8 @@ public class PersonResourceTest {
         phones2.add(t4);
         p2.setPhones(phones2);
 
-<<<<<<< HEAD
-        h1 = new Hobby( "3D-udskrivning", "https://en.wikipedia.org/wiki/3D_printing", p1);
-        h2 = new Hobby( "Akrobatik", "https://en.wikipedia.org/wiki/Acrobatics", p1);
-=======
         h1 = new Hobby("3D-udskrivning", "https://en.wikipedia.org/wiki/3D_printing", p1);
         h2 = new Hobby("Akrobatik", "https://en.wikipedia.org/wiki/Acrobatics", p1);
->>>>>>> c05468913c0cb9ea5599d5be78f2f2c1177b5dc0
 
         hobbies1.add(h1);
         hobbies1.add(h2);
@@ -124,38 +119,16 @@ public class PersonResourceTest {
             em.persist(c1);
             em.persist(p1);
             em.getTransaction().commit();
-<<<<<<< HEAD
-=======
             em.getTransaction().begin();
             em.persist(a2);
             em.persist(p2);
             em.getTransaction().commit();
->>>>>>> c05468913c0cb9ea5599d5be78f2f2c1177b5dc0
         } finally {
             em.close();
         }
     }
 
     @Test
-<<<<<<< HEAD
-    public void testServerIsUp() {
-        System.out.println("Test server is running (okay)");
-        given().when().get("/person").then().statusCode(200);
-    }
-
-    @Test
-    void getAll() {
-        given()
-                .contentType(MediaType.APPLICATION_JSON)
-                .get("/person/all").then()
-                .assertThat()
-                .statusCode(HttpStatus.OK_200.getStatusCode())
-                .body("all", hasSize(1));
-    }
-
-    @Test
-    void getOnId() {
-=======
     public void testAll() {
         given()
                 .contentType(MediaType.APPLICATION_JSON)
@@ -167,43 +140,11 @@ public class PersonResourceTest {
 
     @Test
     public void findById() {
->>>>>>> c05468913c0cb9ea5599d5be78f2f2c1177b5dc0
         given()
                 .contentType(MediaType.APPLICATION_JSON)
                 .get("/person/" + p1.getId()).then()
                 .assertThat()
                 .statusCode(HttpStatus.OK_200.getStatusCode())
-<<<<<<< HEAD
-                .body("fName", equalTo(p1.getFirstName()));
-    }
-
-    //@Test
-    void create() {
-        PersonDTO expected = new PersonDTO(p2);
-
-        given()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(expected)
-                .when()
-                .post("/person").then()
-                .assertThat()
-                .statusCode(HttpStatus.OK_200.getStatusCode());
-    }
-
-    //@Test
-    void edit() {
-        PersonDTO expected = new PersonDTO(p2);
-
-        given()
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(expected)
-                .when()
-                .post("/person").then()
-                .assertThat()
-                .statusCode(HttpStatus.OK_200.getStatusCode());
-    }
-}
-=======
                 .body("fName", equalTo("Mogens"));
     }
 
@@ -227,4 +168,3 @@ public class PersonResourceTest {
     }
 
 }
->>>>>>> c05468913c0cb9ea5599d5be78f2f2c1177b5dc0
